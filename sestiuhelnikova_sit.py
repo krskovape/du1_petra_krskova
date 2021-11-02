0c46a9b14e690aadb0b6bc25ae330d7ad90a4d0d
Petra Krsková <krskovape@natur.cuni.cz>
2021-11-02 12:33:13 +0100

    Dokončení hry na šestiúhelníkové síti

--- sestiuhelnikova_sit.py
+++ sestiuhelnikova_sit.py
@@ -1,11 +1,20 @@
-from turtle import *
+from turtle import speed, penup, pendown, setpos, forward, left, right, write, pensize, circle, exitonclick
 from math import sqrt
 
 speed(0)
 
-p_sloupcu=3
-p_radku=3
-strana=60
+print("Vítej u hry PIŠKVORKY")
+
+#zadání počtu řádků, sloupců a velikosti strany políčka
+p_radku = int(input("Zadej počet řádků herního pole: "))
+p_sloupcu = int(input("Zadej počet sloupců herního pole: "))
+strana = 60
+
+#ošetření toho, že zadaný počet řádků a sloupců není nulový nebo záporný
+while (p_radku<1) or (p_sloupcu<1):
+    print("Neplatná velikost hracího pole, zadej nové:")
+    p_radku = int(input("Počet řádků herního pole: "))
+    p_sloupcu = int(input("Počet sloupců herního pole: "))
 
 #posunutí počátku sítě
 penup()
@@ -36,10 +45,41 @@ penup()
 setpos(a,b)
 pendown()
 
-rad=1
-sloup=3
-x= a + ((strana/2) + 1.5*strana*(sloup-1))
-y= b + strana*(sloup) + 1.5*strana*(rad-1)
-setpos(x, y)
+#samotná hra
+p_tahu=p_radku*p_sloupcu
+
+for i in range(1, p_tahu+1):   
+    #vypsání tahu
+    print("Tah",i,"z",p_tahu)
+
+    #zjištění, který hráč je na tahu a výzva k zadání souřadnic
+    if (i%2!=0):
+        print("Hráči 1, zadej souřadnice pro svůj tah:")
+    elif (i%2==0):
+        print("Hráči 2, zadej souřadnice pro svůj tah:")
+    
+    #načtení souřadnic od hráče a ošetření toho, že nejsou mimo hrací pole
+    rad = int(input("řádek: "))
+    sloup = int(input("sloupec: "))
+    while (rad<1) or (rad>p_radku) or (sloup<1) or (sloup>p_sloupcu):
+        print("Souřadnice jsou mimo hrací pole, zadej nové:")
+        rad = int(input("řádek: "))
+        sloup = int(input("sloupec: "))
+
+    #vykreslení křížku nebo kolečka podle pořadí hráče
+    penup()
+    x= a + ((strana/2) + 1.5*strana*(sloup-1))
+    y= b + (sqrt(3)/2.3)*strana*(sloup) + (sqrt(3))*strana*(rad-1)
+    setpos(x, y)
+    pendown()
+    if (i%2!=0):
+        write("X", align="center", font=["Arial", 30, "normal"])
+    elif (i%2==0):
+        pensize(3)
+        circle(20)
+    penup()       
+    setpos(a,b)
+
+print("KONEC HRY")
 
 exitonclick()
